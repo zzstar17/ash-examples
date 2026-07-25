e4a776275cecbd8de2421b572c74d484388a3587
zstar17 <zzzzzstar17@gmail.com>
2026-07-25 23:40:23 +0100

    Fix compute dispatch read after write sync

--- src/render/command_pools/compute.rs
+++ src/render/command_pools/compute.rs
@@ -84,9 +84,10 @@ impl ComputeCommandBufferPool {
       if queues.transfer.family_index != queues.compute.family_index {
         let acquire = vk::BufferMemoryBarrier2 {
           src_access_mask: vk::AccessFlags2::empty(), // ownership acquire
-          dst_access_mask: vk::AccessFlags2::SHADER_READ,
+          dst_access_mask: vk::AccessFlags2::SHADER_READ.bitor(vk::AccessFlags2::TRANSFER_READ),
           src_stage_mask: vk::PipelineStageFlags2::empty(), // ownership acquire
-          dst_stage_mask: vk::PipelineStageFlags2::COMPUTE_SHADER,
+          dst_stage_mask: vk::PipelineStageFlags2::COMPUTE_SHADER
+            .bitor(vk::PipelineStageFlags2::TRANSFER),
           src_queue_family_index: queues.transfer.family_index,
           dst_queue_family_index: queues.compute.family_index,
           buffer: data.particles_new,
@@ -115,20 +116,25 @@ impl ComputeCommandBufferPool {
     // wait previous dispatch
     let cur_buffer_size = data.current_particles_size();
     if cur_buffer_size > 0 {
+      // wait for previous dispatch write
       let read_wait = vk::BufferMemoryBarrier2 {
-        src_access_mask: vk::AccessFlags2::SHADER_WRITE.bitor(vk::AccessFlags2::SHADER_READ),
-        dst_access_mask: vk::AccessFlags2::SHADER_READ,
+        src_access_mask: vk::AccessFlags2::SHADER_WRITE,
+        dst_access_mask: vk::AccessFlags2::SHADER_READ.bitor(vk::AccessFlags2::TRANSFER_READ),
         src_stage_mask: vk::PipelineStageFlags2::COMPUTE_SHADER,
-        dst_stage_mask: vk::PipelineStageFlags2::COMPUTE_SHADER,
+        dst_stage_mask: vk::PipelineStageFlags2::COMPUTE_SHADER.bitor(vk::PipelineStageFlags2::TRANSFER),
         buffer: data.particles_compute[read_i],
         offset: 0,
         size: cur_buffer_size,
         ..Default::default()
       };
+      // wait for any previous reads or writes
       let write_wait = vk::BufferMemoryBarrier2 {
-        src_access_mask: vk::AccessFlags2::SHADER_WRITE.bitor(vk::AccessFlags2::SHADER_READ),
+        src_access_mask: vk::AccessFlags2::SHADER_WRITE
+          .bitor(vk::AccessFlags2::SHADER_READ)
+          .bitor(vk::AccessFlags2::TRANSFER_READ),
         dst_access_mask: vk::AccessFlags2::SHADER_WRITE.bitor(vk::AccessFlags2::TRANSFER_READ),
-        src_stage_mask: vk::PipelineStageFlags2::COMPUTE_SHADER,
+        src_stage_mask: vk::PipelineStageFlags2::COMPUTE_SHADER
+          .bitor(vk::PipelineStageFlags2::TRANSFER),
         dst_stage_mask: vk::PipelineStageFlags2::COMPUTE_SHADER
           .bitor(vk::PipelineStageFlags2::TRANSFER),
         buffer: data.particles_compute[write_i],
