2afccf7fccad36b55f92e0f01a90dfa56b11b417
z17 <zzzzzstar17@gmail.com>
2026-09-21 11:02:36 +0100

    Add new linux compile shaders

--- compile_shaders.sh
+++ compile_shaders.sh
@@ -6,3 +6,12 @@ VK_ENV="vulkan1.3"
 
 glslc -O $DIR/src/render/shaders/shader.vert --target-env=$VK_ENV -o $DIR/shaders/vert.spv
 glslc -O $DIR/src/render/shaders/shader.frag --target-env=$VK_ENV -o $DIR/shaders/frag.spv
+
+glslc -O $DIR/src/render/shaders/shader.vert --target-env=$VK_ENV -o $DIR/shaders/vert_debug.spv -g
+glslc -O $DIR/src/render/shaders/shader.frag --target-env=$VK_ENV -o $DIR/shaders/frag_debug.spv -g
+
+dxc -spirv -T ps_6_0 -E main $DIR/src/render/shaders/slug_pixel_shader.hlsl -Fo $DIR/shaders/slug_pixel.spv
+dxc -spirv -T vs_6_0 -E main $DIR/src/render/shaders/slug_vertex_shader.hlsl -Fo $DIR/shaders/slug_vertex.spv
+
+dxc -spirv -T ps_6_0 -E main $DIR/src/render/shaders/slug_pixel_shader.hlsl -Fo $DIR/shaders/slug_pixel_debug.spv -Zi -fspv-debug=vulkan-with-source
+dxc -spirv -T vs_6_0 -E main $DIR/src/render/shaders/slug_vertex_shader.hlsl -Fo $DIR/shaders/slug_vertex_debug.spv -Zi -fspv-debug=vulkan-with-source
